7d04844b9dfdc7197ad24b7c621046a7b56fdd99
Masaaki618 <masaaki_618@icloud.com>
2022-11-06 22:41:05 +0900

    Add: 登録機能の実装

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import styled from "@emotion/styled";
 import { Container, TextField, Box, Button } from "@mui/material";
+import { useState } from "react";
 
 export const App = () => {
   const SIncompEriaTodo = styled.div`
@@ -16,34 +17,58 @@ export const App = () => {
     padding: 20px 20px 0;
   `;
 
+  //インプットエリアの初期値
+  const [inputText, SetInputText] = useState("");
+
+  //未完了エリアのtodoに登録する初期値
+  const [imCompTodo, setImCompTodo] = useState([]);
+
+  const onChangeInput = (e) => {
+    //テキスト入力が行わるる毎にinputTextの値を書き換える
+    SetInputText(e.target.value);
+  };
+
+  //エンターが押された時の挙動
+  const onSubmitInput = (e) => {
+    //ページ遷移使用する挙動をキャンセル
+    e.preventDefault();
+    //inputエリアが空白の場合は登録できないようにする
+    if (inputText !== "") {
+      setImCompTodo((prevTodo) => [
+        ...prevTodo,
+        { text: inputText, id: imCompTodo.length + 1 },
+      ]);
+      SetInputText("");
+    }
+  };
+
   return (
     <Container>
       <h2>TODOリスト</h2>
-      <form>
-        <TextField id="standard-basic" label="TODOを入力" variant="standard" />
+      <form onSubmit={onSubmitInput}>
+        <TextField
+          id="standard-basic"
+          label="TODOを入力"
+          variant="standard"
+          value={inputText}
+          onChange={onChangeInput}
+        />
       </form>
       <Box sx={{ display: "flex", justifyContent: "space-around", mt: 5 }}>
         <SIncompEriaTodo>
           <p style={{ margin: "0" }}>未完了エリア</p>
           <ul>
-            <li style={{ marginBottom: "10px" }}>
-              リスト1
-              <Button variant="contained" sx={{ ml: 4 }}>
-                完了
-              </Button>
-              <Button variant="contained" color="error" sx={{ mx: 1 }}>
-                削除
-              </Button>
-            </li>
-            <li style={{ marginBottom: "10px" }}>
-              リスト2
-              <Button variant="contained" sx={{ ml: 4 }}>
-                完了
-              </Button>
-              <Button variant="contained" color="error" sx={{ mx: 1 }}>
-                削除
-              </Button>
-            </li>
+            {imCompTodo.map((todo) => (
+              <li style={{ marginBottom: "10px" }}>
+                {todo.text}
+                <Button variant="contained" sx={{ ml: 4 }}>
+                  完了
+                </Button>
+                <Button variant="contained" color="error" sx={{ mx: 1 }}>
+                  削除
+                </Button>
+              </li>
+            ))}
           </ul>
         </SIncompEriaTodo>
         <SCompEriaTodo>
